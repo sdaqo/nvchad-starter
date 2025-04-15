7e45f620eb349729d9c4f15ee082ff92220dcec2
siduck <siduck@tutanota.com>
2025-04-15 07:55:31 +0530

    use vim.lsp.config & vim.lsp.enable

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,24 +1,10 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "clangd", "pyright", "intelephense", "yamlls", "nil_ls", "gopls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
-lspconfig.omnisharp.setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-    cmd = { "omnisharp-mono" },
+local servers = {
+  "html", "cssls", "ts_ls", "clangd",
+  "pyright", "intelephense", "yamlls",
+  "nil_ls", "gopls"
 }
+
+vim.lsp.config.omnisharp.cmd = { "omnisharp-mono" }
+vim.lsp.enable(servers)
