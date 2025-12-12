973dad7a4aa3162a2955e17ed5dc1698205d4a76
sdaqo <sdaqo.dev@protonmail.com>
2025-12-12 17:33:29 +0100

    add alacritty auto padding autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,9 @@
-require "nvchad.autocmds"
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("VimEnter", {
+  command = ":silent !alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID options 'window.padding.x=0' 'window.padding.y=0' 'window.dynamic_padding=false'",
+})
+
+autocmd("VimLeavePre", {
+  command = ":silent !alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID -r",
+})
