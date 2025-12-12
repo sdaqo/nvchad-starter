d01da403cde73abd6773fbe56d9dfa0b066f73d3
sdaqo <sdaqo.dev@protonmail.com>
2025-12-12 17:42:49 +0100

    some adjustments

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,7 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("VimEnter", {
-  command = ":silent !alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID options 'window.padding.x=0' 'window.padding.y=0' 'window.dynamic_padding=false'",
+  command = ":silent !alacritty msg --socket $ALACRITTY_SOCKET config -w $ALACRITTY_WINDOW_ID options 'window.padding.x=0' 'window.padding.y=0' 'window.dynamic_padding=false' 'font.offset.x=0' 'font.offset.y=1' 'decoration=None'",
 })
 
 autocmd("VimLeavePre", {
